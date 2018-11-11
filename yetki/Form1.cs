6276d24ffafe16c6a080b007c41e7e7c527f0341
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2018-11-11 03:07:48 +0300

    Add project files.

---
+++ yetki/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yetki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Veritabanı1.accdb");
+        public string yetki;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                bool kontrol = false;
+                OleDbCommand komut = new OleDbCommand();
+                komut.Connection = con;
+                komut.CommandText = "Select * from kullanıcılar Where k_adi='" + KullaniciAdi.Text + "' and k_sifre='" + sfr.Text + "' ";
+                OleDbDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    yetki = oku["yetki"].ToString();
+                    kontrol = true;
+                }
+                con.Close();
+                if (kontrol == true)
+                {
+                    Form2 an = new Form2();
+                    an.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Gerekli Bilgileri Doğru Giriniz.", "Ebubekir Yazılım");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ yetki/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace yetki
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Form1 f1=(Form1)Application .OpenForms ["Form1"];
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            if (f1.yetki == "0") 
+            {
+                button1.Hide();
+                button3.Hide();
+                button4.Hide();
+            }
+            if (f1.yetki == "1") 
+            {
+                button2.Hide();
+                button3.Hide();
+                button4.Hide();
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
